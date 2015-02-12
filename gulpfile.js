fcc95cae8cddad73d71c4b2203e6308b2dfe5a79
mcnamee <matt@mcnamee.co>
2015-02-13 08:33:29 +1000

    + Updated readme and gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 /*!
  * gulp
- * $ sudo npm install gulp-sass gulp-minify-html gulp-autoprefixer gulp-minify-css gulp-jshint gulp-uglify gulp-rename gulp-livereload gulp-rev gulp-rev-replace del --save-dev
+ * $ sudo npm install gulp-sass gulp-autoprefixer gulp-minify-css gulp-rename gulp-livereload --save-dev
  */
 
 /*  Load plugins 
@@ -9,16 +9,8 @@
         sass = require('gulp-sass'),
         minifycss = require('gulp-minify-css'),
         autoprefixer = require('gulp-autoprefixer'),
-        jshint = require('gulp-jshint'),
-        uglify = require('gulp-uglify'),
         rename = require('gulp-rename'),
-        concat = require('gulp-concat'),
-        livereload = require('gulp-livereload'),
-        usemin = require('gulp-usemin'),
-        del = require('del'),
-        minifyHTML = require('gulp-minify-html'),
-        rev = require('gulp-rev'),
-        revReplace = require('gulp-rev-replace');
+        livereload = require('gulp-livereload');
 
 
 /*  Styles
